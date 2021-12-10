9f398b2e1ef026551d6fe0985607d2a6fd710216
NandhiniShanmugam14 <nandhini.shanmugam@kanini.com>
2021-12-10 14:46:34 +0530

    Splash Screen Animation

--- app/src/main/java/com/example/petadoptionapplication/SplashActivity.kt
+++ app/src/main/java/com/example/petadoptionapplication/SplashActivity.kt
@@ -1,10 +1,16 @@
 package com.example.petadoptionapplication
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,21 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 4000)
+
+        val topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        val image = findViewById<ImageView>(R.id.imageView2)
+        val text1 = findViewById<TextView>(R.id.textView2)
+        val text2 = findViewById<TextView>(R.id.textView3)
+
+        image.animation = topAnim
+        text1.animation = bottomAnim
+        text2.animation = bottomAnim
+
+
+
+
     }
 }
