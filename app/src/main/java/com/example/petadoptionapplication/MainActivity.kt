4cc4be2ed08141fd6c4379e6886774407a2284b5
Balasubramaniyan Raguraman <balasubramaniyan.raguraman@kanini.com>
2021-12-12 13:21:17 +0530

    Login(Updated)

--- app/src/main/java/com/example/petadoptionapplication/MainActivity.kt
+++ app/src/main/java/com/example/petadoptionapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.petadoptionapplication
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/petadoptionapplication/data/ApiInterface.kt
+++ app/src/main/java/com/example/petadoptionapplication/data/ApiInterface.kt
@@ -1,14 +1,13 @@
 package com.example.petadoptionapplication.data
 
+import android.content.SharedPreferences
 import com.example.petadoptionapplication.data.user.User
 import com.example.petadoptionapplication.data.user.UserReq
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiInterface {
-
     @Headers("Content-Type: application/json")
     @POST("login")
      fun userLogin(@Body user: UserReq): Call<User>
-
 }
