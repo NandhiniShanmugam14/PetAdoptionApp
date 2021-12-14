e249e5d08bac6511a3d65337a711b355f7794520
NandhiniShanmugam14 <nandhini.shanmugam@kanini.com>
2021-12-14 15:05:13 +0530

    Pet Interests

--- app/src/main/java/com/example/petadoptionapplication/ui/interest/InteresredFragment.kt
+++ app/src/main/java/com/example/petadoptionapplication/ui/interest/InteresredFragment.kt
@@ -5,7 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.petadoptionapplication.R
+import com.example.petadoptionapplication.data.PetApplication
+import com.example.petadoptionapplication.ui.home.RecyclerAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class InteresredFragment : Fragment() {
 
@@ -15,7 +23,23 @@ class InteresredFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_interesred, container, false)
-        
+
+        val adapter = InterestAdapter()
+        val recyclerView = view.findViewById<RecyclerView>(R.id.petInterestView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val petApplication = activity?.application as PetApplication
+        val petService = petApplication.api
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val decodedpetsInterests = petService.getPetInterests()
+            val decodedpets = petService.getPets()
+
+            withContext(Dispatchers.Main)
+            {
+                adapter.setData(decodedpets.pets,decodedpetsInterests.petInterests)
+            }
+        }
 
         return view
     }
